a83868ecbc3894a22314213499f07cfaf875225d
FireFlyForLife <maiko.s.99@gmail.com>
2015-12-24 21:38:41 +0100

    PUUUSH

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Jails;
+using Jails.Isolators.AppDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: AllowPartiallyTrustedCallers]
+namespace ConsoleApplication1
+{ 
+    public static class WinAPI
+    {
+        [DllImport("User32.dll")]
+        
+        public static extern long SetCursorPos(int x, int y);
+    }
+
+    class Program
+    {
+        
+
+        static void Main(string[] args) {
+            var appOptions = new AppDomainOptions();
+            appOptions.Permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.UnmanagedCode));
+
+            var isolator = new AppDomainIsolator(appOptions);
+            var environment = new DefaultEnvironment();
+            environment.Register(@"C:\Users\Maiko\Documents\GitHubVisualStudio\RSMacroProject\ConsoleApplication1\Untrusted\bin\Debug\Untrusted.dll");
+
+            using (var jail = Jail.Create(isolator, environment)) {
+                dynamic proxy = jail.Resolve("Untrusted.Class1");
+                proxy.test();
+                proxy.move();
+            }
+        }
+
+        
+    }
+}
---
+++ ConsoleApplication1/Untrusted/Class1.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untrusted
+{
+    public class Class1
+    {
+        public void test() {
+            Console.WriteLine("Test");
+        }
+
+        
+        public void move() {
+            WinAPI.SetCursorPos(10, 10);
+        }
+    }
+}
